dc3556ae47ef4507585b76df81e0c46a2b9414c9
Bryan Haskin <bryan@ocgcreative.com>
2014-06-03 09:46:24 -0700

    initial commit

---
+++ bootstrapModal/module.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once get_template_directory() . '/lib/module.php';
+
+class bootstrapModal extends OneModule{
+  public $version = "1.0";
+  public $level = "normal";
+  public $priority = "1";
+
+}
